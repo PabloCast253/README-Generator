1ad828c392b07a79012b7c041bff9318e0a78d8a
unknown <pablocast253@gmail.com>
2024-12-05 15:49:07 -0800

    README code finished

--- Develop/index.js
+++ Develop/index.js
@@ -1,17 +1,93 @@
 // TODO: Include packages needed for this application
-
+import inquirer from "inquirer";
+import fs from "fs";
+import generateMarkdown from "./utils/generateMarkdown.js";
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?"
+},
+{
+    type: "input",
+    name: "description",
+    message: "Provide a short description for your project"
+},
+{
+    type: "input",
+    name: "motivation",
+    message: "What was your motivation for making this project"
+},
+{
+    type: "input",
+    name: "problemsSolves",
+    message: "What problems does this application solve"
+},
+{
+    type: "input",
+    name: "learned",
+    message: "What did you learn while making this application"
+},
+{
+    type: "input",
+    name: "installation",
+    message: "What are the installation instructions needed to run your project "
+},
+{
+    type: "input",
+    name: "usage",
+    message: "Provide instructions and examples for usage of this application include screenshots/links to video as needed"
+},
+{
+    type: "input",
+    name: "collaborators",
+    message: "please name and collabortors that helped write the code and their github or email as well",
+},
+{
+    type: "list",
+    name: "license",
+    message: "Choose a license for your project:",
+    choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3-Clause", "None"],
+},
+{
+    type: "input",
+    name: "contributions",
+    message: "What are the contribution guidlines"
+},
+{
+    type: "input",
+    name: "tests",
+    message: "Provide some tests for you application and how to run the tests"
+},
+
+{
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?",
+},
+{
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+},];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log("README.md generated!")
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            const markdown = generateMarkdown(answers);
+            writeToFile("README.md", markdown);
+        })
+        .catch((err) => console.error(err));
+}
 
 // Function call to initialize app
 init();
-
-console.log();
-
-
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,80 @@
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  // check if there is even a licence in the first place if there isnt a license provided return an empty string to add to the README 
+  if(!license){
+    return " ";
+  }
+  // return the license with a badge string 
+  return `![License](https://img.shields.io/badge/License-${license.replace(/ /g, '%20')}-blue.svg)`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license){
+    return '';
+  }
+  return `[Learn more about ${license}](https://choosealicense.com/licenses/)`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) return '';
+  return `## License
+This project is licensed under the ${license} license.  
+${renderLicenseLink(license)}`;
+}
 
 // TODO: Create a function to generate markdown for README
+// essnetially creating the text that the read me is going to have 
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Table of Contents
+- [Description](#Description)
+  * motivation for making project
+  * problems this project solves 
+  * Things learned while making this project
+- [Installation](#installation)
+- [Usage](#usage)
+- [Collaborators](#Collaborators)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Description
+${data.description}
+  - ${data.motivation}
+  - ${data.problemsSolves}
+  - ${data.learned}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Collaborators
+${data.collaborators} 
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions, feel free to reach out:  
+- GitHub: [${data.github}](https://github.com/${data.github})  
+- Email: [${data.email}](mailto:${data.email})
 `;
 }
 
